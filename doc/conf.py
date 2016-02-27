b1049aa9bbf8524bc3ea623796370f9521f6679e
Matthias Geier <Matthias.Geier@gmail.com>
2016-02-27 13:40:38 +0100

    DOC: Switch to bizstyle theme

--- doc/conf.py
+++ doc/conf.py
@@ -39,6 +39,7 @@ except Exception:
 # -- Options for HTML output ----------------------------------------------
 
 html_title = project + ' version ' + release
+html_theme = 'bizstyle'
 
 # -- Options for LaTeX output ---------------------------------------------
 
