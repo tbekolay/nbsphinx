d391393b0749d7e648ee540cca16e09186cbe61b
Trevor Bekolay <tbekolay@gmail.com>
2016-04-07 09:36:33 -0400

    Rendered docs

---
+++ _sources/index.txt
@@ -0,0 +1,24 @@
+.. include:: ../README.rst
+
+All content shown below -- except for the section :doc:`rst` -- was
+generated from Jupyter notebooks.
+
+.. toctree::
+    :maxdepth: 3
+    :glob:
+
+    usage.ipynb
+    markdown-cells.ipynb
+    code-cells.ipynb
+    raw-cells.ipynb
+    pre-executed.ipynb
+    allow-errors.ipynb
+    timeout.ipynb
+    hidden-cells.ipynb
+    subdir/*
+    rst.rst
+    links.ipynb
+
+.. only:: html
+
+    There is also :ref:`orphan.ipynb`, just for the sake of it.
---
+++ conf.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Use sphinx-quickstart to create your own conf.py file!
+# After that, you have to edit a few things.  See below.
+
+# Select nbsphinx and, if needed, add a math extension (mathjax or pngmath):
+extensions = [
+    'nbsphinx',
+    'sphinx.ext.mathjax',
+]
+
+# Exclude build directory and Jupyter backup files:
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
+
+# Default language for syntax highlighting in reST and Markdown cells
+highlight_language = 'none'
+
+# -- These set defaults that can be overridden through notebook metadata --
+
+# See http://nbsphinx.readthedocs.org/en/latest/allow-errors.html
+# and http://nbsphinx.readthedocs.org/en/latest/timeout.html for more details.
+
+# If True, the build process is continued even if an exception occurs:
+#nbsphinx_allow_errors = True
+
+# Controls when a cell will time out (defaults to 30; use -1 for no timeout):
+#nbsphinx_timeout = 60
+
+# Default Pygments lexer for syntax highlighting in code cells
+#nbsphinx_codecell_lexer = 'ipython3'
+
+# -- The settings below this line are not specific to nbsphinx ------------
+
+master_doc = 'index'
+
+project = 'nbsphinx'
+author = 'Matthias Geier'
+copyright = '2016, ' + author
+
+linkcheck_ignore = [r'http://localhost:\d+/']
+
+# -- Get version information from Git -------------------------------------
+
+try:
+    from subprocess import check_output
+    release = check_output(['git', 'describe', '--tags', '--always'])
+    release = release.decode().strip()
+except Exception:
+    release = '<unknown>'
+
+# -- Options for HTML output ----------------------------------------------
+
+html_title = project + ' version ' + release
+
+# -- Options for LaTeX output ---------------------------------------------
+
+latex_elements = {
+    'papersize': 'a4paper',
+    'preamble': r"""
+\usepackage{lmodern}  % heavier typewriter font
+""",
+}
+
+latex_documents = [
+    (master_doc, 'nbsphinx.tex', project, author, 'howto'),
+]
+
+latex_show_urls = 'footnote'
